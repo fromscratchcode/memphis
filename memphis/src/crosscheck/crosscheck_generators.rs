c1de6d10581374ea30990e460c7f06f5a47625bc
Tyler Green <tyler@fromscratchcode.com>
2026-07-29 11:45:32 -0400

    Add generator chain crosscheck test

--- memphis/src/crosscheck/crosscheck_generators.rs
+++ memphis/src/crosscheck/crosscheck_generators.rs
@@ -47,3 +47,28 @@ b = next(g)
 
     assert_stop_iteration!(e.exception);
 }
+
+#[test]
+fn generator_chain() {
+    let input = r#"
+def numbers():
+    yield 1
+    yield 2
+    yield 3
+
+def doubled(values):
+    for value in values:
+        yield value * 2
+
+def plus_one(values):
+    for value in values:
+        yield value + 1
+
+step1 = numbers()
+step2 = doubled(step1)
+step3 = plus_one(step2)
+
+list(step3)
+"#;
+    assert_crosscheck_return!(input, list![int!(3), int!(5), int!(7)]);
+}
